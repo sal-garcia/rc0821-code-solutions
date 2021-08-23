429a67a3df151998adcc59c6518ed3ba00949471
sal garcia <salgarcia940@gmail.com>
2021-08-23 11:36:00 -0700

    operator and expressions completed

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,42 @@
+var height = 2;
+var width = 5;
+
+var area = width * height;
+console.log('value ', area);
+console.log('type of ', typeof area);
+
+var bill = 5;
+var payment = 6;
+
+var change = payment - bill;
+console.log('value ', change);
+console.log('type of ', typeof change);
+
+var quizzes = 3;
+var midterm = 3;
+var final = 5;
+var grade = midterm + final + quizzes / 3;
+console.log('value ', grade);
+console.log('typeof', typeof grade);
+
+var firstName = 'sal ';
+var lastName = 'garcia ';
+var fullName = firstName + lastName;
+
+console.log('value ', fullName);
+console.log('type of ', typeof fullName);
+
+var pH = 2;
+var isAcidic = pH < 7;
+console.log('value ', isAcidic);
+console.log('type of ', typeof isAcidic);
+
+var headCount = 5;
+var isSparta = headCount === 300;
+console.log('value', isSparta);
+console.log('type of ', typeof isSparta);
+
+var motto = fullName;
+motto += 'is the GOAT';
+console.log('value', motto);
+console.log('type of ', typeof motto);
