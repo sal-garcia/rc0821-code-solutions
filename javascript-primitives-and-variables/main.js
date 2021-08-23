ae4b2a4a4af6e5f76bd007af912d57aa0cc4f4e4
sal garcia <salgarcia940@gmail.com>
2021-08-23 10:25:46 -0700

    javascript primitives and variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,16 @@
+var fullName = 'sal garcia';
+console.log('type of ' + typeof fullName);
+
+var isCool = true;
+console.log('type of ' + typeof isCool);
+
+var totalPets = 2;
+console.log('type of ' + typeof totalPets);
+
+var unknown = null;
+console.log('value ' + unknown);
+console.log('type of ' + typeof null);
+
+var never;
+console.log('value ' + never);
+console.log('type of ' + typeof never);
