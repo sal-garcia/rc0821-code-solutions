69afb464479ef87c643b785a0ae32c55755542c3
sal garcia <salgarcia940@gmail.com>
2021-08-24 14:56:13 -0700

    completed javascript functinos practice

---
+++ javascript-functions-practice/get-first-character.js
@@ -0,0 +1,5 @@
+/* exported getFirstCharacter */
+function getFirstCharacter(string) {
+  var first = string[0];
+  return first;
+}
---
+++ javascript-functions-practice/get-second-character.js
@@ -0,0 +1,5 @@
+/* exported getSecondCharacter */
+function getSecondCharacter(string) {
+  var second = string[1];
+  return second;
+}
