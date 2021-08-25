e0b35d2a5246a0765fa62f7ac4d2d13d407a1c97
sal garcia <salgarcia940@gmail.com>
2021-08-25 10:53:55 -0700

    javascript-methods/

---
+++ javascript-methods/main.js
@@ -0,0 +1,52 @@
+var one = 1;
+var two = 2;
+var three = 3;
+var maximumValue = Math.max(one, two, three);
+console.log(maximumValue);
+
+var heroes = ['superman', 'batman', 'spiderman', 'greenlantern'];
+var randomNumber = Math.random(heroes);
+randomNumber += randomNumber * heroes.length;
+var randomIndex = Math.floor(randomNumber);
+console.log(randomIndex);
+var randomHero = heroes[randomIndex];
+console.log(randomHero);
+
+var library = [{
+  title: 'harrypotter',
+  author: 'J.K. rowling'
+},
+{
+  title: 'Outsiders',
+  author: 'S.E Hinton'
+},
+{
+  title: 'giver',
+  author: 'Lois Lowry'
+}];
+
+var lastBook = library.pop();
+console.log(lastBook);
+
+var firstBook = library.shift();
+console.log(firstBook);
+
+var js = {
+  title: 'JavaScript for Impatient Programmers',
+  author: 'Dr. Axel Rauschmayer'
+};
+var css = {
+  title: 'CSS Secrets',
+  author: 'Lea Verou'
+};
+library.push(js);
+library.unshift(css);
+library.splice(1, 1);
+console.log(library);
+
+var fullName = 'sal garcia';
+var firstAnsLastName = fullName.split(' ');
+console.log(firstAnsLastName);
+var firstName = firstAnsLastName[0];
+var sayMyName = firstName.toUpperCase();
+console.log(sayMyName);
