a992377711f02311aa38fe109bdd9677fac630e4
sal garcia <salgarcia940@gmail.com>
2021-08-27 15:42:32 -0700

    completed string manipulation 1 fixed

---
+++ string-manipulation-1/src/get-first-char.js
@@ -0,0 +1,11 @@
+/* exported getFirstChar */
+function getFirstChar(string) {
+  var first = string[0];
+  return first;
+}
+
+/*
+1. define a variable to store the value
+2. assign that variable the value of the string at index0
+3. return the storage variable
+*/
