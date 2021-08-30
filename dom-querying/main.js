c2a19654028898ba356304535e0e9343996f6cea
sal garcia <salgarcia940@gmail.com>
2021-08-30 10:59:16 -0700

    completed dom querying

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -1,10 +1,15 @@
 console.log('hello world');
-console.log(document.querySelector('h1'));
-console.dir('h1');
-console.log(document.querySelector('#explanation'));
-console.dir('#explanation');
-console.log(document.querySelector('.hint'));
-console.log(document.querySelector('.hint'));
-console.dir('.hint');
+var first = document.querySelector('h1');
+console.log(first);
+console.dir(first);
+
+var second = document.querySelector('#explanation');
+console.log(second);
+console.dir(second);
+
+var third = document.querySelector('.hint');
+console.log(third);
+console.dir(document.querySelector('.hint'));
+
 console.log(document.querySelectorAll('p'));
 console.log(document.querySelectorAll('.example-link'));
