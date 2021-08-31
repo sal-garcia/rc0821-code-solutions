1f2bbf3266da12a899355bd506920b45a8e79779
sal garcia <salgarcia940@gmail.com>
2021-08-31 14:49:05 -0700

    completed javascript lightbulb

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -6,9 +6,14 @@ $background.addEventListener('click', blackened);
 
 function blackened(event) {
   click++;
-  if (click !== 0) {
-    $background.className += ' black-background';
-    $lightBulb.className += ' clicked-light-bulb';
-
+  if (click % 2 === 1) {
+    $background.classList.remove('white');
+    $background.classList.add('black-background');
+    $lightBulb.classList.add('clicked-light-bulb');
+  } else if (click % 2 === 0) {
+    $background.classList.remove('black-background');
+    $background.classList.add('white');
+    $lightBulb.classList.remove('clicked-light-bulb');
+    $lightBulb.className.add('yellow');
   }
 }
