54d045c6e5a239890c38ebd57c89424c4a9f000b
sal garcia <salgarcia940@gmail.com>
2021-09-01 10:01:53 -0700

    work in progress

--- array-manipulation-2/src/drop.js
+++ array-manipulation-2/src/drop.js
@@ -1,9 +1,17 @@
 /* exported drop */
 function drop(array, count) {
   var container = [];
-  if (array > 0) {
 
-    container.push(array.indexOf([count]));
+  for (var i = count; i < array.length; i++) {
+    container.push(array[i]);
+
   }
   return container;
 }
+
+/*
+1. add a container variable with the value of an empty array
+2. use a for loop to go from count to the end of the array
+3.push each [i] element from count to the end one by one
+4.return the container array
+*/
--- array-manipulation-2/src/includes.js
+++ array-manipulation-2/src/includes.js
@@ -1,1 +1,17 @@
 /* exported includes */
+function includes(array, value) {
+  var boolean = false;
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      boolean = true;
+    }
+  }
+  return boolean;
+}
+/*
+1. assign a storage variable for a boolean set to false
+2.use a for loop to go thru each element in the array
+3. in the for loop code block add an if conditional statement
+that checks if any [i] element of the array is equal to the value
+4.if it is add update the boolean value to be true
+*/
