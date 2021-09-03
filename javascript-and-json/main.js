ced7b3b74c1d7a15e3120fbb1e4d55ad30b9b597
sal garcia <salgarcia940@gmail.com>
2021-09-03 11:34:56 -0700

    completed javascript and json

---
+++ javascript-and-json/main.js
@@ -0,0 +1,28 @@
+var array = [{
+  title: 'Hatchet',
+  isbn: '9780027701302',
+  author: 'Gary Paulsen'
+},
+{
+  title: 'Giver',
+  isbn: '9780007141425',
+  author: 'Lois Lowry'
+
+},
+{
+  title: 'Outsiders',
+  isbn: '9780140385724',
+  author: 'S.E Hinton'
+}];
+console.log(typeof array, array);
+
+console.log(JSON.stringify(array));
+console.log(typeof JSON.stringify(array));
+
+var stringThree = '{ "id": 36844 }';
+console.log(stringThree);
+console.log(typeof stringThree);
+
+var obj = JSON.parse(stringThree);
+console.log(obj);
+console.log(typeof obj);
