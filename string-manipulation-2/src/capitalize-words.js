ff30f6d994c66fb829e431f84a5ba1bc145be4d2
sal garcia <salgarcia940@gmail.com>
2021-09-13 22:44:19 -0700

    string manipulation 2 completed

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,1 +1,8 @@
 /* exported capitalizeWords */
+function capitalizeWords(string) {
+  var splittingString = string.split(' ');
+  for (var i = 0; i < splittingString.length; i++) {
+    splittingString[i] = splittingString[i][0].toUpperCase() + splittingString[i].substr(1).toLowerCase();
+  }
+  return splittingString.join(' ');
+}
